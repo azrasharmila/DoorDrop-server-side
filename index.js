41c6e92a1df77dddbd0eb2c0742ac25e89e3e09e
azrasharmila <azrasharmila06@gmail.com>
2026-02-26 16:03:36 +0600

    Implemented detete parcel  funtionality on my parcels

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 3000
 
@@ -30,48 +30,60 @@ async function run() {
 
     //parcel api
 
-     app.get('/parcels', async (req, res) => {
-            const query = {}
-            const {email} = req.query;
-            // /parcels?email=''&
-            if(email){
-                query.senderEmail = email;
-            }
+    app.get('/parcels', async (req, res) => {
+      const query = {}
+      const { email } = req.query;
+      // /parcels?email=''&
+      if (email) {
+        query.senderEmail = email;
+      }
+
+      const options = { sort: { createdAt: -1 } }
 
 
-            const cursor = parcelsCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+      const cursor = parcelsCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
 
-    app.get('/parcels', async(req,res)=>{
+    app.get('/parcels', async (req, res) => {
 
     })
 
 
 
 
-     app.post('/parcels', async (req, res) => {
-            const parcel = req.body;
-            const result = await parcelsCollection.insertOne(parcel);
-            res.send(result)
-        })
+    app.post('/parcels', async (req, res) => {
+      const parcel = req.body;
+      parcel.createdAt = new Date();
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result)
+    })
+
+
+    app.delete('/parcels/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const result = await parcelsCollection.deleteOne(query);
+      res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
-    
+
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('doordrop is running')
+  res.send('doordrop is running')
 })
 
 app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
