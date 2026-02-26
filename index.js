6a816f0c68a46b4b16cb4da776e1556934d8e1d1
azrasharmila <azrasharmila06@gmail.com>
2026-02-26 15:20:56 +0600

    Project setup+ implemented parcel get and post APIs

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qnleitt.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    await client.connect();
+
+    const db = client.db('door_drop_db');
+    const parcelsCollection = db.collection('parcels');
+
+
+    //parcel api
+
+     app.get('/parcels', async (req, res) => {
+            const query = {}
+            const {email} = req.query;
+            // /parcels?email=''&
+            if(email){
+                query.senderEmail = email;
+            }
+
+
+            const cursor = parcelsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+    app.get('/parcels', async(req,res)=>{
+
+    })
+
+
+
+
+     app.post('/parcels', async (req, res) => {
+            const parcel = req.body;
+            const result = await parcelsCollection.insertOne(parcel);
+            res.send(result)
+        })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('doordrop is running')
+})
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
